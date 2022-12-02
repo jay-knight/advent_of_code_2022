c340b1836c50096488f405f19ae01a1c8e47d41c
Jay Knight <jay.knight@stjude.org>
2022-12-02 12:33:32 -0600

    3rd Star

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,51 @@ use std::path::Path;
 
 fn main() {
     // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("./1.input") {
+    if let Ok(lines) = read_lines("./2.input") {
         // Consumes the iterator, returns an (Optional) String
-        let mut first = 0;
-        let mut second = 0;
-        let mut third = 0;
 
         let mut total = 0;
+
         for line in lines {
             if let Ok(line_str) = line {
-                if line_str == "" {
-                    if total > first {
-                        third = second;
-                        second = first;
-                        first = total;
-                    } else if total > second {
-                        third = second;
-                        second = total;
-                    } else if total > third {
-                        third = total;
-                    }
-                    total = 0;
+                let plays: Vec<&str> = line_str.split(" ").collect();
+                let opp_play = match plays[0] {
+                    "A" => "R",
+                    "B" => "P",
+                    "C" => "S",
+                    _   => "XXX"
+                };
+                let my_play = match plays[1] {
+                    "X" => "R",
+                    "Y" => "P",
+                    "Z" => "S",
+                    _   => "XXX"
+                };
+                let bonus = match my_play {
+                    "R" => 1,
+                    "P" => 2,
+                    "S" => 3,
+                    _   => 9999
+                };
+                let outcome: u16;
+                if my_play == opp_play {
+                    outcome = 3;
                 } else {
-                    let number = line_str.parse::<i32>().unwrap();
-                    total += number;
-                    println!("{} -> {}", line_str, total);
+                    outcome = match (opp_play, my_play) {
+                        ("R", "P") => 6,
+                        ("R", "S") => 0,
+                        ("P", "R") => 0,
+                        ("P", "S") => 6,
+                        ("S", "R") => 6,
+                        ("S", "P") => 0,
+                        _ => 8888
+                    };
                 }
+                let score = bonus + outcome;
+                total += score;
+                println!("{} : {}-{} ({} + {} = {}) -> {}", line_str, opp_play, my_play, bonus, outcome, score, total);
             }
         }
-        println!("{} + {} + {} = {}", first, second, third, first + second + third);
     }
 }
 
