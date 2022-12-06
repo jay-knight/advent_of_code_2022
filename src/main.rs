d61a04ea0c3066a9291cfb033a003062a5032c1c
Jay Knight <jay.knight@stjude.org>
2022-12-05 21:35:58 -0600

    9th Star

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,55 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use regex::Regex;
 
 fn main() {
     // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("./4.input") {
+    if let Ok(lines) = read_lines("./5.input") {
         // Consumes the iterator, returns an (Optional) String
 
-        let mut total = 0;
+        // Hard-code initial configuration
+        let mut stacks: Vec<Vec<char>> = Vec::new();
+        stacks.push("NCRTMZP".chars().collect());
+        stacks.push("DNTSBZ".chars().collect());
+        stacks.push("MGQRFCTG".chars().collect());
+        stacks.push("GRZ".chars().collect());
+        stacks.push("ZNRH".chars().collect());
+        stacks.push("FHSWPZLD".chars().collect());
+        stacks.push("WDZRCGM".chars().collect());
+        stacks.push("SJFLHWZQ".chars().collect());
+        stacks.push("SQPWN".chars().collect());
+        println!("Initial:");
+        print_stacks(&stacks);
+
+        let move_re = Regex::new(r"^move (\d+) from (\d+) to (\d+)").unwrap();
 
         for line in lines {
             if let Ok(line_str) = line {
-                let ranges: Vec<&str> = line_str.split(",").collect();
-
-                let first_range: Vec<&str> = ranges[0].split("-").collect();
-                let second_range: Vec<&str> = ranges[1].split("-").collect();
                 println!("{}", line_str);
-                println!("{:?}", first_range);
-                println!("{:?}", second_range);
-                if first_range[0].parse::<u32>().unwrap() >=
-                    second_range[0].parse::<u32>().unwrap() &&
-                    first_range[0].parse::<u32>().unwrap() <=
-                    second_range[1].parse::<u32>().unwrap()
-                    ||
-                    first_range[1].parse::<u32>().unwrap() >=
-                    second_range[0].parse::<u32>().unwrap() &&
-                    first_range[1].parse::<u32>().unwrap() <=
-                    second_range[1].parse::<u32>().unwrap()
-                    ||
-                    second_range[0].parse::<u32>().unwrap() >=
-                    first_range[0].parse::<u32>().unwrap() &&
-                    second_range[1].parse::<u32>().unwrap() <=
-                    first_range[1].parse::<u32>().unwrap()
-                {
-                        total += 1;
-                        println!("They overlap! {}", total);
-                } else {
-                        println!("They don't overlap!");
-                }
+                let move_cap = move_re.captures(&line_str).unwrap();
+                let count = move_cap.get(1).unwrap().as_str().parse::<u32>().unwrap();
+                let source = move_cap.get(2).unwrap().as_str().parse::<usize>().unwrap();
+                let dest = move_cap.get(3).unwrap().as_str().parse::<usize>().unwrap();
+                //println!("{} {} {}", count, source, dest);
 
+                for _ in 1..=count {
+                    let moving = stacks[source - 1].pop().unwrap();
+                    stacks[dest - 1].push(moving);
+                }
+                print_stacks(&stacks);
             }
         }
+    }
+}
+
+fn print_stacks(stacks: &Vec<Vec<char>>) {
+    for (i, stack) in stacks.iter().enumerate() {
+        print!("{}: ", i+1);
+        for character in stack {
+            print!("{} ", character);
+        }
+        println!("");
     }
 }
 
