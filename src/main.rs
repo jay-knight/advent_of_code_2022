9aeee9bb223a0434f00662469ff752e0012199f4
Jay Knight <jay.knight@stjude.org>
2022-12-05 23:38:28 -0600

    12th Star

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     for (i,c) in input.chars().enumerate() {
         last_four.push(c);
         println!("{} {c}", i+1);
-        if last_four.len() < 4 {
+        if last_four.len() < 14 {
             continue;
         }
 
@@ -20,9 +20,19 @@ fn main() {
         set.insert(last_four[1]);
         set.insert(last_four[2]);
         set.insert(last_four[3]);
+        set.insert(last_four[4]);
+        set.insert(last_four[5]);
+        set.insert(last_four[6]);
+        set.insert(last_four[7]);
+        set.insert(last_four[8]);
+        set.insert(last_four[9]);
+        set.insert(last_four[10]);
+        set.insert(last_four[11]);
+        set.insert(last_four[12]);
+        set.insert(last_four[13]);
 
         println!("{}", set.len());
-        if set.len() == 4 {
+        if set.len() == 14 {
             println!("Found! {}", i);
             break;
         }
@@ -31,6 +41,16 @@ fn main() {
         last_four[0] = last_four[1];
         last_four[1] = last_four[2];
         last_four[2] = last_four[3];
+        last_four[3] = last_four[4];
+        last_four[4] = last_four[5];
+        last_four[5] = last_four[6];
+        last_four[6] = last_four[7];
+        last_four[7] = last_four[8];
+        last_four[8] = last_four[9];
+        last_four[9] = last_four[10];
+        last_four[10] = last_four[11];
+        last_four[11] = last_four[12];
+        last_four[12] = last_four[13];
         _ = last_four.pop();
 
     }
