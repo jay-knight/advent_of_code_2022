b7459ac07d73a3c85506511ac79d505c0c57a34b
Jay Knight <jay.knight@stjude.org>
2022-12-09 21:48:23 -0600

    18th Star

--- src/main.rs
+++ src/main.rs
@@ -38,11 +38,13 @@ impl Position {
 
 fn main() {
 
-    let mut head_position = Position { x: 0, y: 0 }; 
-    let mut tail_position = Position { x: 0, y: 0 }; 
+    let mut knots: Vec<Position> = Vec::new();
+    for _ in 0..=9 {
+        knots.push(Position { x: 0, y: 0 });
+    }
 
     let mut tail_positions: HashSet<Position> = HashSet::new();
-    tail_positions.insert(tail_position);
+    tail_positions.insert(knots[9]);
 
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./9.input") {
@@ -55,29 +57,31 @@ fn main() {
                 for _ in 0..steps {
                     // move the head
                     match direction.as_str() {
-                        "U" => head_position.up(),
-                        "D" => head_position.down(),
-                        "L" => head_position.left(),
-                        "R" => head_position.right(),
+                        "U" => knots[0].up(),
+                        "D" => knots[0].down(),
+                        "L" => knots[0].left(),
+                        "R" => knots[0].right(),
                         _ => println!("WHHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT?"),
                     }
-                    println!("New head_position: {}, {}", head_position.x, head_position.y);
-                    let diff = head_position.diff(&tail_position);
-                    println!("Diff {}, {}", diff.x, diff.y);
+                    //println!("New head_position: {}, {}", head_position.x, head_position.y);
+                    for k in 1..=9 {
+                        let diff = knots[k-1].diff(&knots[k]);
+                        //println!("Diff {}, {}", diff.x, diff.y);
 
-                    if diff.x.abs() + diff.y.abs() == 3 {
-                        println!("Need to move diagonally");
-                        tail_position.step(
-                                diff.x / diff.x.abs(),
-                                diff.y / diff.y.abs(),
-                            )
-                    } else if diff.x.abs() == 2  || diff.y.abs() == 2 {
-                        // Need to move one step
-                        println!("Need to move one step");
-                        tail_position.step(diff.x / 2, diff.y / 2)
+                        if diff.x.abs() + diff.y.abs() == 3 {
+                            println!("Need to move diagonally");
+                            knots[k].step(
+                                    diff.x / diff.x.abs(),
+                                    diff.y / diff.y.abs(),
+                                )
+                        } else if diff.x.abs() == 2  || diff.y.abs() == 2 {
+                            // Need to move one step
+                            println!("Need to move one step");
+                            knots[k].step(diff.x / 2, diff.y / 2)
+                        }
                     }
-                    tail_positions.insert(tail_position);
-                    println!("New tail: {}, {}", tail_position.x, tail_position.y);
+                    tail_positions.insert(knots[9]);
+                    //println!("New tail: {}, {}", tail_position.x, tail_position.y);
                 }
             }
         }
