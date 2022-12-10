440a90d66ea59e03f4df1317467c17c94ec22cf7
Jay Knight <jay.knight@stjude.org>
2022-12-10 16:23:24 -0600

    19th Star

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,65 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashSet;
+use regex::RegexSet;
 
-#[derive(PartialEq, Eq, Hash, Copy, Clone)]
-struct Position {
-    x: i32,
-    y: i32,
+struct Computer {
+    clock: i32,
+    xreg: i32,
+    total_signal_strength: i32,
 }
 
-impl Position {
-    fn up(&mut self) {
-        self.step(0, 1);
+impl Computer {
+    fn tick(&mut self) {
+        self.clock += 1;
+        if (self.clock + 20) % 40 == 0 {
+            self.add_signal_strength();
+            println!("Clock: {}, X: {}: SS: {}", self.clock, self.xreg, self.total_signal_strength);
+        }
     }
-    fn down(&mut self) {
-        self.step(0, -1);
+    fn add_signal_strength(&mut self) {
+        self.total_signal_strength += self.clock * self.xreg;
     }
-    fn left(&mut self) {
-        self.step(-1, 0);
-    }
-    fn right(&mut self) {
-        self.step(1, 0);
-    }
-    fn step(&mut self, x: i32, y: i32) {
-        self.x += x;
-        self.y += y;
+    fn noop(&mut self) {
+        self.tick();
     }
 
-    fn diff(&self, other: &Position) -> Position {
-        let other = Position {
-            x: self.x - other.x,
-            y: self.y - other.y,
-        };
-        return other;
+    fn addx(&mut self, arg: i32) {
+        self.tick();
+        self.tick();
+        self.xreg += arg;
     }
 }
 
 fn main() {
+    let mut compy = Computer{clock: 0, xreg: 1, total_signal_strength: 0};
 
-    let mut knots: Vec<Position> = Vec::new();
-    for _ in 0..=9 {
-        knots.push(Position { x: 0, y: 0 });
-    }
-
-    let mut tail_positions: HashSet<Position> = HashSet::new();
-    tail_positions.insert(knots[9]);
-
+    let set = RegexSet::new(&[
+        r"^noop$",
+        r"^addx -?\d+$",
+    ]).unwrap();
     // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("./9.input") {
-
+    if let Ok(lines) = read_lines("./10.input") {
         for line in lines {
             if let Ok(line_str) = line {
-                let parts: Vec<String> = line_str.split(" ").map(|s| s.to_string()).collect();
-                let (direction, steps) = (&parts[0], parts[1].parse::<u32>().unwrap());
-                println!("{line_str} -> step {direction} {steps} times");
-                for _ in 0..steps {
-                    // move the head
-                    match direction.as_str() {
-                        "U" => knots[0].up(),
-                        "D" => knots[0].down(),
-                        "L" => knots[0].left(),
-                        "R" => knots[0].right(),
-                        _ => println!("WHHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT?"),
-                    }
-                    //println!("New head_position: {}, {}", head_position.x, head_position.y);
-                    for k in 1..=9 {
-                        let diff = knots[k-1].diff(&knots[k]);
-                        //println!("Diff {}, {}", diff.x, diff.y);
-
-                        if diff.x.abs() + diff.y.abs() == 3 {
-                            println!("Need to move diagonally");
-                            knots[k].step(
-                                    diff.x / diff.x.abs(),
-                                    diff.y / diff.y.abs(),
-                                )
-                        } else if diff.x.abs() == 2  || diff.y.abs() == 2 {
-                            // Need to move one step
-                            println!("Need to move one step");
-                            knots[k].step(diff.x / 2, diff.y / 2)
-                        }
-                    }
-                    tail_positions.insert(knots[9]);
-                    //println!("New tail: {}, {}", tail_position.x, tail_position.y);
+                let matches:Vec<_> = set.matches(&line_str).into_iter().collect();
+                println!("{line_str} {:?}", matches);
+                match matches[0] {
+                    0 => compy.noop(),
+                    1 => {
+                        let arg = line_str
+                            .split(" ")
+                            .map(|s| s.to_string())
+                            .collect::<Vec<String>>()[1]
+                            .parse::<i32>()
+                            .unwrap();
+                        compy.addx(arg);
+                    },
+                    _ => panic!("Unexpected input!"),
                 }
             }
         }
     }
-    println!("Tail positions: {:?}", tail_positions.len());
 }
 
 
